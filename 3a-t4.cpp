d3b08f36f06085f04c48820c0faf0da0c75cbe38
BoArSt <145919015+BoArSt@users.noreply.github.com>
2023-10-28 12:03:50 +0300

    Update 3a-t4.cpp

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -1,16 +1,18 @@
-#include<iostream> 
-using namespace std; 
+#include<iostream>
+using namespace std;
 
 int main()
 {
-int k, i = 1, sum_k = 0, number;
-cout << " Enter the amount of integers to sum = "; cin >> k;
-do {
-cout << "Enter integer nr. " << i << ": "; 
-cin >> number;
-sum_k += number; i++;
-} while (i <= k);
-cout << " The total sum of " << k << " integers is: "  << sum_k;
-return 0;
+	int k, i = 1, sum_k = 0, number;
+	cout << " Enter the amount of integers to sum = "; cin >> k;
+	if (k == 0) {
+		cout << "Error" << endl;
+	} else
+	do {
+		cout << "Enter integer nr. " << i << ": ";
+		cin >> number;
+		sum_k += number; i++;
+	} while (i <= k);
+	cout << " The total sum of " << k << " integers is: " << sum_k;
+	return 0;
 }
-
